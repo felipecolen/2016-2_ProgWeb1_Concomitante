61d2a159a2cc60e5230d1a88137caf3b5b021420
Felipe Oliveira Colen <felipecolen@gmail.com>
2016-11-02 17:16:53 -0400

    add aula 3

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
  
 # 2016-2 Programação para Web I - Concomitante
 Materiais/Códigos utilizados e/ou desenvolvidos na disciplina de Programação Web I no Curso Técnico em Informática para Internet Concomitante do IFRO _Campus_ Porto Velho Zona Norte, utilizando o microframework [Flask](http://flask.pocoo.org/) ([Python](https://www.python.org/))
+
+
+## Downloads dos códigos
+Você pode clonar esse repositório ou fazer o download individual das aulas. Lembre-se de descompactar o arquivo depois.
+
+###[Aula 2 - 01/11/2016](aula02_flask_01-11-2016.zip)
+###[Aula 3 - 03/11/2016](aula03_flask_03-11-2016.zip)
---
+++ aula03_flask_03-11-2016/aula03_flask.py
@@ -0,0 +1,59 @@
+import datetime, pymysql
+from flask import Flask
+from flask.globals import request
+from flask.templating import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    # por padrão, os templates tem que estar na pasta templates
+    return render_template('index.html')
+
+
+@app.route('/contato', methods=['GET', 'POST'])
+def cadastra_contato():
+
+    # se clicou no botão "enviar"
+    if request.method == 'POST':
+        nome = request.form['nome']
+        msg = request.form['mensagem']
+        data_e_hora = datetime.datetime.now()  # pega a data e hora atuais
+
+        return """
+            <br><br>
+            TOP! <br><br>
+            DEU CERTO! <br><br>
+            ISSO FOI UM POST! <br><br><br>
+
+            Nome: <h1> {} </h1> <br>
+            Msg: <h2> {} </h2> <br>
+            às {}
+        """.format(nome, msg, data_e_hora)
+
+
+    return render_template('contato.html')
+
+
+@app.route('/conectar', methods=['GET'])
+def conectar_ao_banco():
+
+    # conexão mysql
+    conexao = pymysql.connect(
+        # host='127.0.0.1',
+        host='172.17.0.2',
+        user='root',
+        passwd='senhamysql',
+        db='db_nome_do_db'
+    )
+    conexao.cursor()
+
+    return "Conectou com Sucesso!!!  :D"
+
+
+if __name__ == '__main__':
+    # enquanto estiver desenvolvendo,
+    # utilize o debug=True para visualizar os erros e
+    # seu servidor embutido reiniciar automaticamente
+    app.run(debug=True)
